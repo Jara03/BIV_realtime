05fa2cebe446aaf5e4676272eda5d25d47a69e06
Rania <raniabensaid.rbs@gmail.com>
2023-04-05 17:08:33 +0200

    ajout minutage des passage (débug a voir)

--- test.js
+++ test.js
@@ -5,54 +5,78 @@
 const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const fs = require("fs")
 const readline = require("readline");
+const Long = require("long");
+
 
 //fonction de chargement des données avec express
 
-async function formatGTFSName(zenbusName){
-    const linedata = fs.createReadStream("./verdun-rezo/routes.txt", {encoding:"utf8"});
+ function formatGTFSName(zenbusName){
+     return new Promise((resolve, reject) => {
+         const linedata = fs.createReadStream("./verdun-rezo/routes.txt", {encoding: "utf8"});
 
-    //fetch chaque ligne et split a a virgule
-    const rl = readline.createInterface({
-        input: linedata,
-        crlfDelay: Infinity
-    });
+         let res = '';
 
-    rl.on('line', (line) => {
+         const rl = readline.createInterface({
+             input: linedata,
+             crlfDelay: Infinity
+         });
 
-        const values = line.split(',');
-        if(zenbusName.toString() === (values[0])){
-            console.log(values[2])
-           // console.log(values[0]);
+         rl.on('line', (line) => {
+             const values = line.split(',');
+             if (zenbusName.toString() === values[0]) {
+                 res = values[2];
+             }
+         });
 
-        }
-    });
+         rl.on('close', () => {
+             resolve(res);
+         });
+     });
+}
 
-    rl.on('close', () => {});
-
-
+ function formatRemainingMinutes(nextDate){
+    const now = new Date();
+     // Unix timestamp
+     const diffInSeconds = (nextDate.getTime() - now.getTime()) / 1000;
+    return Math.trunc(diffInSeconds / 60);
 }
 
 async function run(){
-    const data = fs.readFileSync("./pill.proto") //attention le fichier doit etre en UTF-8
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data);
+    const data = fs.readFileSync("./poll.proto") //attention le fichier doit etre en UTF-8
+    const feed1 = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data);
+    const feed = feed1.toJSON();
 
-    console.log(feed.entity.length)
 
-//console.log(feed.entity[0])
-    for(let i = 0; i < feed.entity.length-1;i++){
+    for(let i = 0; i < feed.entity.length;i++){
 
         if(feed.entity[i].vehicle){
 
-            await formatGTFSName(feed.entity[i].vehicle.trip.routeId)
+             await formatGTFSName(feed.entity[i].vehicle.trip.routeId).then(r => console.log(r))
 
 
-        }else if(feed.entity[i].tripUpdate){
+        }else
+        if (feed.entity[i].tripUpdate) {
 
-            await formatGTFSName(feed.entity[i].tripUpdate.trip.routeId)
+            if (feed.entity[i].tripUpdate.stopTimeUpdate[0].arrival) { //problème avec les accès
+                console.log(i)
+                console.log(feed.entity[1].tripUpdate.stopTimeUpdate[1].arrival)
+                let value = feed.entity[i].tripUpdate.stopTimeUpdate[0].arrival.time.toString()//prochain passage a l'arret 0 chercher le bon arret
+
+                const longValue = Long.fromString(value); // Create a Long object from a string
+                const timestamp = longValue.toNumber() * 1000; // Convert the Long object to a timestamp in milliseconds
+                const date = new Date(timestamp);
+
+
+                let test = await formatGTFSName(feed.entity[i].tripUpdate.trip.routeId);
+
+                console.log(test + ' dans :' + formatRemainingMinutes(date));
+
+            }
         }
     }
 }
 
+run()
 
 
 
